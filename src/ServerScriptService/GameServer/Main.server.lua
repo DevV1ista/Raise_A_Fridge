2d86914b0bf7831d7fe89193cc6d5a981479a89e
v1ista <colakuvalon@gmail.com>
2026-06-01 21:42:58 +0200

    fix: require server services from parent

--- src/ServerScriptService/GameServer/Main.server.lua
+++ src/ServerScriptService/GameServer/Main.server.lua
@@ -2,14 +2,14 @@ local RunService = game:GetService("RunService")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 local Remotes = require(ReplicatedStorage.Game.Shared.Remotes)
-local PlayerDataService = require(script.Services.PlayerDataService)
-local RollService = require(script.Services.RollService)
-local PlotService = require(script.Services.PlotService)
+local PlayerDataService = require(script.Parent.Services.PlayerDataService)
+local RollService = require(script.Parent.Services.RollService)
+local PlotService = require(script.Parent.Services.PlotService)
 
 print("Raise A Fridge server booting...")
 
-PlayerDataService.Init()
 PlotService.Init()
+PlayerDataService.Init()
 
 local function getState(player)
 	return PlayerDataService.getPublicState(player)
