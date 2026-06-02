8b1c0af5e00ca7595e4365ba2d8a8d145590d42d
v1ista <colakuvalon@gmail.com>
2026-06-02 15:08:48 +0200

    Boot prestige controller on client

--- src/StarterPlayer/StarterPlayerScripts/GameClient/Main.client.lua
+++ src/StarterPlayer/StarterPlayerScripts/GameClient/Main.client.lua
@@ -1,5 +1,9 @@
 print("Raise A Fridge client booting...")
 
 local controllers = script.Parent:WaitForChild("Controllers")
+
 local hudController = require(controllers:WaitForChild("HudController"))
+local prestigeController = require(controllers:WaitForChild("PrestigeController"))
+
 hudController.Start()
+prestigeController.Start()
