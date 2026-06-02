befaf4cc1261df1b4dde42c488a945784719e7b6
v1ista <colakuvalon@gmail.com>
2026-06-02 14:12:16 +0200

    Integrate prestige into progression multipliers

--- src/ServerScriptService/GameServer/Services/ProgressionMultiplierService.lua
+++ src/ServerScriptService/GameServer/Services/ProgressionMultiplierService.lua
@@ -1,16 +1,37 @@
 local ProgressionMultiplierService = {}
 
 ProgressionMultiplierService._mutationService = nil
+ProgressionMultiplierService._prestigeService = nil
+ProgressionMultiplierService._dataService = nil
 
 function ProgressionMultiplierService:Init(services)
 	self._mutationService = services.MutationService
+	self._prestigeService = services.PrestigeService
+	self._dataService = services.DataService
+end
+
+function ProgressionMultiplierService:_getProfile(player)
+	if not self._dataService then
+		return nil
+	end
+
+	if self._dataService.GetProfile then
+		return self._dataService:GetProfile(player)
+	end
+
+	return nil
 end
 
 function ProgressionMultiplierService:GetMoneyMultiplier(player)
 	local multiplier = 1
+	local profile = self:_getProfile(player)
 
 	if self._mutationService then
 		multiplier *= self._mutationService:GetMoneyMultiplier(player)
+	end
+
+	if self._prestigeService and profile then
+		multiplier *= self._prestigeService:GetMoneyMultiplier(profile)
 	end
 
 	return multiplier
@@ -18,9 +39,14 @@ end
 
 function ProgressionMultiplierService:GetXpMultiplier(player)
 	local multiplier = 1
+	local profile = self:_getProfile(player)
 
 	if self._mutationService then
 		multiplier *= self._mutationService:GetXpMultiplier(player)
+	end
+
+	if self._prestigeService and profile then
+		multiplier *= self._prestigeService:GetXpMultiplier(profile)
 	end
 
 	return multiplier
